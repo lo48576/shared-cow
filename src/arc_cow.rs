d8f228eaae4db797665990092788a5bdc16f67a3
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-08-04 13:49:27 +0900

    Implement conversion between `RcCow` and `ArcCow`

--- src/arc_cow.rs
+++ src/arc_cow.rs
@@ -1,5 +1,9 @@
 //! `ArcCow`.
 
+use std::rc::Rc;
+
+use crate::RcCow;
+
 def_shared_cow! {
     #[doc = "[`Cow`][`std::borrow::Cow`] with variant with shared [`Arc`][`std::sync::Arc`] data."]
     pub def ArcCow<B>(std::sync::Arc<B>);
@@ -9,3 +13,22 @@ impl_cow! { ArcCow<B>(std::sync::Arc<B>); <A> }
 impl_str_like! { ArcCow, std::sync::Arc<str>, str, String }
 impl_str_like! { ArcCow, std::sync::Arc<std::path::Path>, std::path::Path, std::path::PathBuf }
 impl_str_like! { ArcCow, std::sync::Arc<std::ffi::OsStr>, std::ffi::OsStr, std::ffi::OsString }
+
+impl<'a, B> ArcCow<'a, B>
+where
+    B: 'a + ToOwned + ?Sized,
+    for<'b> &'b B: Into<Rc<B>>,
+{
+    /// Creates an [`RcCow`] value.
+    pub fn to_rccow(&self) -> RcCow<'a, B> {
+        use std::borrow::Borrow;
+        match *self {
+            ArcCow::Borrowed(b) => RcCow::Borrowed(b),
+            ArcCow::Owned(ref o) => RcCow::Owned(o.borrow().to_owned()),
+            ArcCow::Shared(ref s) => {
+                let b: &B = s.borrow();
+                RcCow::Shared(b.into())
+            },
+        }
+    }
+}
--- src/rc_cow.rs
+++ src/rc_cow.rs
@@ -1,5 +1,9 @@
 //! `RcCow`.
 
+use std::sync::Arc;
+
+use crate::ArcCow;
+
 def_shared_cow! {
     #[doc = "[`Cow`][`std::borrow::Cow`] with variant with shared [`Rc`][`std::rc::Rc`] data."]
     pub def RcCow<B>(std::rc::Rc<B>);
@@ -9,3 +13,22 @@ impl_cow! { RcCow<B>(std::rc::Rc<B>); <A> }
 impl_str_like! { RcCow, std::rc::Rc<str>, str, String }
 impl_str_like! { RcCow, std::rc::Rc<std::path::Path>, std::path::Path, std::path::PathBuf }
 impl_str_like! { RcCow, std::rc::Rc<std::ffi::OsStr>, std::ffi::OsStr, std::ffi::OsString }
+
+impl<'a, B> RcCow<'a, B>
+where
+    B: 'a + ToOwned + ?Sized,
+    for<'b> &'b B: Into<Arc<B>>,
+{
+    /// Creates an [`ArcCow`] value.
+    pub fn to_arccow(&self) -> ArcCow<'a, B> {
+        use std::borrow::Borrow;
+        match *self {
+            RcCow::Borrowed(b) => ArcCow::Borrowed(b),
+            RcCow::Owned(ref o) => ArcCow::Owned(o.borrow().to_owned()),
+            RcCow::Shared(ref s) => {
+                let b: &B = s.borrow();
+                ArcCow::Shared(b.into())
+            },
+        }
+    }
+}
